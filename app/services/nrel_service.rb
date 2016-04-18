db652ec5b691418b744ba91ae0178fa8c4cd7d14
Obtuse <theobtuseautodidact@gmail.com>
2016-04-18 09:40:39 -0600

    trying to hit first endpoint in first test

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -3,6 +3,9 @@ class NrelService
   def initialize
     @connection = Faraday.new("https://developer.nrel.gov//api/alt-fuel-stations/v1")
     connection.params["api_key"] = ENV["api_key"]
-    
+  end
+
+  def nearest(zip)
+    connection.get("/nearest.json?location=#{zip}&distance=6&limit=10&fuel_type=ELEC,LPG")
   end
 end
